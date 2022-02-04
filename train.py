87b1b99338878265cfcaddec4846094db8bb71d6
batuan <thaibatuana1k41pbc@gmail.com>
2022-02-04 14:18:26 +0100

    abc

--- train.py
+++ train.py
@@ -1,44 +1,131 @@
+from ast import arg
+from turtle import st
 import numpy as np
 from model import Resnet1D
 import argparse
 from keras.models import Sequential
 from keras.layers.recurrent import LSTM
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+import tensorflow.keras as keras
 import os
+import keras_resnet.models
+import argparse
 
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  try:
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-  except RuntimeError as e:
-    print(e)
+parser = argparse.ArgumentParser(description='Config Model')
+parser.add_argument('--model-mode', metavar='N', type=int, default=1,
+                    help='1 for LSTM, 2 for CNN, 3 for ResNet')
+parser.add_argument('--batch', type=int, default=4096,
+                    help='batch size)')
+parser.add_argument('--nepoch', type=int, default=20,
+                    help='batch size)')
+parser.add_argument('--model-name', type=str, default='tuan.h5',
+                    help='model save name')
+
+args = parser.parse_args()
 
 
-def get_model(timeseries, nfeatures, nclass):
+
+def set_gpu(devide=1):
+  # limit memory for GPU
+  os.environ["CUDA_VISIBLE_DEVICES"]=str(devide)
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  if gpus:
+    try:
+      for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+      print(e)
+
+def get_model_LSTM(timeseries, nfeatures, nclass):
     model = Sequential()
     model.add(LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True, input_shape=(timeseries, nfeatures)))
-    model.add(LSTM(units=32, dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
+    model.add(LSTM(units=128, dropout=0.1, recurrent_dropout=0.1, return_sequences=False))
     model.add(Dense(units=nclass, activation='softmax'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(48, activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(nclass, activation='softmax'))
     return model
 
 
+
+def get_model_CNN(timeseries, nfeatures, nclass):
+    model = Sequential()
+    input_shape=(timeseries, nfeatures, 1)
+    # 1st conv layer
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape,  padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape,  padding='same'))
+
+    # 2nd conv layer
+    model.add(keras.layers.Conv2D(128, (3, 3), activation='relu',  padding='same'))
+    model.add(keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # 3rd conv layer
+    model.add(keras.layers.Conv2D(256, (5, 5), activation='relu', padding='same'))
+    model.add(keras.layers.MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # flatten output and feed it into dense layer
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(128, activation='relu'))
+
+    # output layer
+    model.add(keras.layers.Dense(nclass, activation='softmax'))
+
+    return model
+  
+def get_model_ResNet(data_shape, nbclass):
+    shape, classes = (data_shape[1], data_shape[2], 1), nbclass
+    x = keras.layers.Input(shape)
+    model = keras_resnet.models.ResNet18(x, classes=classes)
+    return model
+
+def test_model(model):
+  X_test, y_test = np.load('./data_train_test/test_imgs.npy'), np.load('./data_train_test/test_labels.npy')
+  data_shape = X_test.shape
+  X_test = X_test.reshape((data_shape[0], data_shape[1], data_shape[2], 1))
+  y_test = tf.keras.utils.to_categorical(y_test)
+
+  score = model.evaluate(X_test, y_test, verbose=0)
+  print(score)
+
+
 if __name__ == "__main__":
-    data = np.load('./train_imgs.npy')
-    label = np.load('./train_labels.npy')
-    label = label.reshape((len(label), 1))
-    model = get_model(data.shape[1], data.shape[2], 1)
-    X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2, random_state=2018)
+    set_gpu()
+    model_mode = args.model_mode
+
+    data = np.load('./data_train_test/train_imgs.npy')
+    data_shape = data.shape
+    if model_mode != 1:
+      data = data.reshape((data_shape[0], data_shape[1], data_shape[2], 1))
+    label = np.load('./data_train_test/train_labels.npy')
+    label = tf.keras.utils.to_categorical(label) #label.reshape((len(label), 1))
+    X_train, X_val, y_train, y_val = train_test_split(data, label, test_size=0.3, random_state=42)
     # print(y_test)
+
+    if model_mode == 1:
+      model = get_model_LSTM(data.shape[1], data.shape[2], 2)
+    elif model_mode == 2:
+      model = get_model_CNN(data.shape[1], data.shape[2], 2)
+    else:
+      model = get_model_ResNet(data_shape, 2)
 
     model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
     model.summary()
 
-    batch_size = 1024*4
-    nb_epochs = 10
+    batch_size = args.batch
+    nb_epochs = args.nepoch
 
-    model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epochs, validation_data=(X_test, y_test))
-    model.save('tuan.h5')
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epochs, validation_data=(X_val, y_val))
+
+    test_model(model)  
+  
+    model.save(args.model_name)
